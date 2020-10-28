20bc78faaf395b31a08c6da191d9abcfa90b9dfd
AlecTech <eremeevoleg@gmail.com>
2020-10-27 19:52:03 -0600

    Added Storage class and added methods;

---
+++ XUnitCumulativePractice/Storage.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace XUnitCumulativePractice
+{
+    public class Storage
+    {
+        public List<Item> Contents { get; set; } = new List<Item>();
+
+        public Storage()
+        {
+      
+        }
+
+        public void AddItem(Item item)
+        {
+            Contents.Add(item);
+
+        }
+
+        public void RemoveItem()
+        {
+            var last = Contents.LastOrDefault();
+            if (last != null)
+            {
+                Contents.Remove(last);
+            }
+        }
+    }
+}
