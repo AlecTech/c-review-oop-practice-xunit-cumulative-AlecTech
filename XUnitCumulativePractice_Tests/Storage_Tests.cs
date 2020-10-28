83d0bca7da26f86626586a1da7268186b5e3a088
AlecTech <eremeevoleg@gmail.com>
2020-10-27 20:04:26 -0600

    added 1st Fact test;

---
+++ XUnitCumulativePractice_Tests/Storage_Tests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Xunit;
+using XUnitCumulativePractice;
+
+namespace XUnitCumulativePractice_Tests
+{
+    public class Storage_Tests
+    {
+        [Fact]
+        public void AddItem_ShouldAddItem()
+        {
+            // Arrange
+            Item myItem = new Item("testItem");
+            Storage myStorage = new Storage();
+            // Act
+          
+            myStorage.AddItem(myItem);
+
+            // Assert
+            
+            Assert.Single(myStorage.Contents);
+        }
+
+        
+    }
+}
